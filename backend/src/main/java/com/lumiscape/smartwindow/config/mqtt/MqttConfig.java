8207520eb7b1f444036e8d37dfd93d56c91a87b6
곽정현 <kwakjh98@naver.com>
2025-10-31 15:46:21 +0900

    feat: Add initial Mqtt configuration and service

--- backend/src/main/java/com/lumiscape/smartwindow/config/mqtt/MqttConfig.java
+++ backend/src/main/java/com/lumiscape/smartwindow/config/mqtt/MqttConfig.java
@@ -1,4 +1,100 @@
 package com.lumiscape.smartwindow.config.mqtt;
 
+import com.lumiscape.smartwindow.global.infra.MqttPublishService;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.annotation.IntegrationComponentScan;
+import org.springframework.integration.annotation.MessagingGateway;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.config.EnableIntegration;
+import org.springframework.integration.mqtt.core.DefaultMqttPahoClientFactory;
+import org.springframework.integration.mqtt.core.MqttPahoClientFactory;
+import org.springframework.integration.mqtt.inbound.MqttPahoMessageDrivenChannelAdapter;
+import org.springframework.integration.mqtt.outbound.MqttPahoMessageHandler;
+import org.springframework.integration.mqtt.support.DefaultPahoMessageConverter;
+import org.springframework.integration.mqtt.support.MqttHeaders;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHandler;
+import org.springframework.messaging.handler.annotation.Header;
+
+@Configuration
+@EnableIntegration
+@IntegrationComponentScan(basePackageClasses = MqttPublishService.class)
 public class MqttConfig {
+
+    @Value("${mqtt.server.uri}")
+    private String serverUri;
+
+    @Value("${mqtt.client.id}")
+    private String clientId;
+
+    @Value("${mqtt.username}")
+    private String username;
+
+    @Value("${mqtt.password}")
+    private String password;
+
+    public static final String MQTT_OUTBOUND_CHANNEL = "mqttOutboundChannel";
+    public static final String MQTT_INPUT_CHANNEL = "mqttInputChannel";
+
+    @Bean
+    public MqttPahoClientFactory mqttClientFactory() {
+
+        DefaultMqttPahoClientFactory factory = new DefaultMqttPahoClientFactory();
+        MqttConnectOptions options = new MqttConnectOptions();
+
+        options.setServerURIs(new String[]{serverUri});
+        options.setUserName(username);
+        options.setPassword(password.toCharArray());
+        options.setCleanSession(true);
+        options.setAutomaticReconnect(true);
+        options.setKeepAliveInterval(60);
+
+        factory.setConnectionOptions(options);
+
+        return factory;
+    }
+
+    @Bean
+    public MessageChannel mqttOutboundChannel() {
+        return new DirectChannel();
+    }
+
+    @Bean
+    @ServiceActivator(inputChannel = MQTT_OUTBOUND_CHANNEL)
+    public MessageHandler mqttOutbound() {
+        MqttPahoMessageHandler messageHandler = new MqttPahoMessageHandler(clientId + "_outbound", mqttClientFactory());
+
+        messageHandler.setAsync(true);
+        messageHandler.setDefaultQos(1);
+
+        return messageHandler;
+    }
+
+    @MessagingGateway(defaultRequestChannel = MQTT_OUTBOUND_CHANNEL)
+    public interface MqttPublishGateway {
+        void publish(@Header(MqttHeaders.TOPIC) String topic, String payload);
+    }
+
+    @Bean
+    public MessageChannel mqttInputChannel() {
+        return new DirectChannel();
+    }
+
+    @Bean
+    public MqttPahoMessageDrivenChannelAdapter inbound() {
+        String topic = "/devices/+/status/+";
+
+        MqttPahoMessageDrivenChannelAdapter adapter = new MqttPahoMessageDrivenChannelAdapter(clientId + "_inbound", mqttClientFactory(), topic);
+
+        adapter.setCompletionTimeout(5000);
+        adapter.setConverter(new DefaultPahoMessageConverter());
+        adapter.setQos(1);
+        adapter.setOutputChannel(mqttInputChannel());
+
+        return adapter;
+    }
 }
