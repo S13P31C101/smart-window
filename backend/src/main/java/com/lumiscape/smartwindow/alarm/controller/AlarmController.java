fd63e67bfbd677fc4d7321e2367d54ddf58b18a3
곽정현 <226970068+by-k-jh@users.noreply.github.com>
2025-11-14 11:54:59 +0900

    refactor: refactoring alarms part for auth

--- backend/src/main/java/com/lumiscape/smartwindow/alarm/controller/AlarmController.java
+++ backend/src/main/java/com/lumiscape/smartwindow/alarm/controller/AlarmController.java
@@ -7,6 +7,8 @@ import com.lumiscape.smartwindow.alarm.service.AlarmService;
 import com.lumiscape.smartwindow.global.common.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,42 +21,53 @@ public class AlarmController {
     private final AlarmService alarmService;
 
     @GetMapping("/alarms")
-    public ApiResponse<List<AlarmResponse>> getAllUserAlarms( Long userId) {
+    public ApiResponse<List<AlarmResponse>> getAllUserAlarms(@AuthenticationPrincipal UserDetails userDetails) {
+        Long userId = Long.parseLong(userDetails.getUsername());
         List<AlarmResponse> responses = alarmService.getAllUserAlarms(userId);
 
         return ApiResponse.onSuccess(responses);
     }
 
     @PostMapping("/alarms")
-    public ApiResponse<AlarmResponse> createAlarm(Long userId, @RequestBody AlarmCreateRequest request) {
+    public ApiResponse<AlarmResponse> createAlarm(@AuthenticationPrincipal UserDetails userDetails,
+                                                  @RequestBody AlarmCreateRequest request) {
+        Long userId = Long.parseLong(userDetails.getUsername());
         AlarmResponse newAlarm = alarmService.createAlarm(userId, request);
 
         return ApiResponse.onSuccess(HttpStatus.CREATED, newAlarm);
     }
 
     @GetMapping("/alarms/{alarm-id}")
-    public ApiResponse<AlarmResponse> getAlarmById( Long userId, @PathVariable("alarm-id") Long alarmId) {
+    public ApiResponse<AlarmResponse> getAlarmById(@AuthenticationPrincipal UserDetails userDetails,
+                                                   @PathVariable("alarm-id") Long alarmId) {
+        Long userId = Long.parseLong(userDetails.getUsername());
         AlarmResponse response = alarmService.getAlarmById(userId, alarmId);
 
         return ApiResponse.onSuccess(response);
     }
 
     @PatchMapping("/alarms/{alarm-id}")
-    public ApiResponse<AlarmResponse> updateAlarm(Long userId, @PathVariable("alarm-id") Long alarmId, @RequestBody AlarmUpdateRequest request) {
+    public ApiResponse<AlarmResponse> updateAlarm(@AuthenticationPrincipal UserDetails userDetails,
+                                                  @PathVariable("alarm-id") Long alarmId, @RequestBody AlarmUpdateRequest request) {
+        Long userId = Long.parseLong(userDetails.getUsername());
         AlarmResponse response = alarmService.updateAlarm(userId, alarmId, request);
 
         return ApiResponse.onSuccess(response);
     }
 
     @DeleteMapping("/alarms/{alarm-id}")
-    public ApiResponse<?> deleteAlarm( Long userId, @PathVariable("alarm-id") Long alarmId) {
+    public ApiResponse<?> deleteAlarm(@AuthenticationPrincipal UserDetails userDetails,
+                                      @PathVariable("alarm-id") Long alarmId) {
+        Long userId = Long.parseLong(userDetails.getUsername());
         alarmService.deleteAlarm(userId, alarmId);
 
         return ApiResponse.onSuccess();
     }
 
     @GetMapping("/devices/{device-id}/alarms")
-    public ApiResponse<List<AlarmResponse>> getAlarmsByDevice( Long userId, @PathVariable("device-id") Long deviceId) {
+    public ApiResponse<List<AlarmResponse>> getAlarmsByDevice(@AuthenticationPrincipal UserDetails userDetails,
+                                                              @PathVariable("device-id") Long deviceId) {
+        Long userId = Long.parseLong(userDetails.getUsername());
         List<AlarmResponse> responses = alarmService.getAlarmsByDevice(userId, deviceId);
 
         return ApiResponse.onSuccess(responses);
