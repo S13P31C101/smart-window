ebc3b2c354792f83787dbcff7143e21aaadfbc11
곽정현 <226970068+by-k-jh@users.noreply.github.com>
2025-11-17 15:20:12 +0900

    feature: Add various type of ai gen

--- backend/src/main/java/com/lumiscape/smartwindow/media/domain/MediaOrigin.java
+++ backend/src/main/java/com/lumiscape/smartwindow/media/domain/MediaOrigin.java
@@ -2,5 +2,13 @@ package com.lumiscape.smartwindow.media.domain;
 
 public enum MediaOrigin {
     ORIGINAL,
-    AI_GENERATED
+    AI_RP,
+    AI_SUNSET,
+    AI_DAWN,
+    AI_AFTERNOON,
+    AI_NIGHT,
+    AI_RP_SUNSET,
+    AI_RP_DAWN,
+    AI_RP_AFTERNOON,
+    AI_RP_NIGHT
 }
